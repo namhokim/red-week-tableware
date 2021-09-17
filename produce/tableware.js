97cf4f928e7e14b8d02078d02060dbccc4985eab
김남호 <nhkimnamo@gmail.com>
2021-09-18 08:42:43 +0900

    feature: serve one fix dish
    - add url "/order/dish"

--- produce/tableware.js
+++ produce/tableware.js
@@ -3,9 +3,18 @@ const http = require('http');
 const host = 'localhost';
 const port = 8000;
 
+const dish = {id: 'eb646a86-c948-4b1f-b275-18b9880646eb', type: 'Paper Plates', style: 'Colored Glass Plates'};
+
 const requestListener = function (req, res) {
-    res.writeHead(200);
-    res.end("Hello server!");
+    switch (req.url) {
+        case '/order/dish':
+            res.writeHead(200, {'Content-Type': 'application/json'});
+            res.end(JSON.stringify(dish));
+            break;
+        default:
+            res.end();
+            break;
+    }
 };
 
 const server = http.createServer(requestListener);
