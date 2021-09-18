567cda2d0a02d5cca13ef3cebbdb1e741eee55f6
namo <nhkimnamo@gmail.com>
2021-09-18 09:33:17 +0900

    feature: add url matches order/dishes/{size}

--- produce/tableware.js
+++ produce/tableware.js
@@ -12,6 +12,18 @@ class DishMachine {
     createDish() {
         return {id: this.uuidV4(), type: this.types(), style: this.styles()};
     }
+    createDishes(size, response) {
+        response.write('[');
+        let i;
+        for (i = 1; i <= size; i++) {
+            let dish = this.createDish();
+            response.write(JSON.stringify(dish));
+            if (i < size) {
+                response.write(",\n");
+            }
+        }
+        response.write(']');
+    }
     uuidV4() {
         return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
             let r = Math.random() * 16 | 0, v = c === 'x' ? r : (r & 0x3 | 0x8);
@@ -37,6 +49,12 @@ const requestListener = function (req, res) {
             const dish = machine.createDish();
             res.writeHead(200, {'Content-Type': 'application/json'});
             res.end(JSON.stringify(dish));
+            break;
+        case (req.url.match(/\/order\/dishes/) || {}).input:
+            let dishSize = req.url.match(/\d+/);
+            res.writeHead(200, {'Content-Type': 'application/json'});
+            machine.createDishes(dishSize, res);
+            res.end();
             break;
         default:
             res.end();
