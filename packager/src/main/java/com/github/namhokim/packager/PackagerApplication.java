1397775ff551cd736c50bd80e7c99bad78b60a92
namo <nhkimnamo@gmail.com>
2021-09-18 18:27:33 +0900

    init packager by start.spring.io
    - Project: Gradle Project
    - Language: Java
    - Spring Boot: 2.5.4
    - Dependencies: Lombok, Spring Web
    
    ref. https://start.spring.io/

---
+++ packager/src/main/java/com/github/namhokim/packager/PackagerApplication.java
@@ -0,0 +1,13 @@
+package com.github.namhokim.packager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PackagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PackagerApplication.class, args);
+	}
+
+}
